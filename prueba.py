f67ef3055db6bbcc93069292a76aef1acdbff14b
Oscar David Zendejas Rangel <oscar.zendejas@avahi.ai>
2026-05-20 14:39:48 -0600

    new arithmetic operations added to Calculator Inc project

--- prueba.py
+++ prueba.py
@@ -4,8 +4,23 @@ import os
 def suma(a,b):
     return a + b
 
+def resta(a,b):
+    return a - b
+
+def multiplicacion(a,b):
+    return a * b
+
+def division(a,b):
+    if b != 0:
+        return a / b
+    else:
+        return "Cannot divide by zero"
+
 if __name__ == "__main__":
     print("Hola Mundo")
     print(suma(5,10))
+    print(resta(10,5))
+    print(multiplicacion(5,10))
+    print(division(10,5))
     print(datetime.datetime.now())
     print(os.listdir())
